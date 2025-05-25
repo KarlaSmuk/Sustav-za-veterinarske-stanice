88853382c308bda8c1b306b1e801835f7960e7a4
LukaZ02 <luka.zaja.net@gmail.com>
2025-05-25 21:49:51 +0200

    Connected Owner to Pets

--- backend/src/models/entities/Owner.entity.ts
+++ backend/src/models/entities/Owner.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, OneToMany, OneToOne, JoinColumn, Column } from 'typeorm';
 import { User } from './User.entity';
+import { Pet } from './Pet.entity';
 
 @Entity()
 export class Owner {
@@ -12,6 +13,9 @@ export class Owner {
     @OneToOne(() => User)
     @JoinColumn({ name: 'user_id' })
     user: User;
+
+    @OneToMany(() => Pet, (pet) => pet.owner)
+    pets!: Pet[];
 
     constructor(user: User) {
         this.user = user;
