9c98ea07cfa2ed0a6820de5bc181d2e7fda5935d
KarlaSmuk <karla.smuk@gmail.com>
2025-05-25 21:22:55 +0200

    added to navigate to owners page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Owners from "./pages/Owners.page";
 import Pets from "./pages/Pets.page";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/owners" replace />} />
       <Route path="/owners" element={<Owners />} />
       <Route path="/owners/:ownerId" element={<Pets />} />
     </Routes>
